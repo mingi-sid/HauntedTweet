3af5cd2b2c5340dbb2c2fc8544be9a3845ec43d0
Mingi Shin <yuagnun@gmail.com>
2018-03-10 21:59:55 +0900

    Add NLP parser and corresponding test code.

--- data/parser.py
+++ data/parser.py
@@ -6,24 +6,52 @@ class Parser():
         self._totalcount = 0.0
         self._count = {}
         self._freq = {}
+        self._tokens = []
         if openfile != None:
             self.file = openfile
 
-    def analyze(self, targetfile):
+    def get_stats(self, targetfile):
         tagger = Twitter()
         for line in self.file:
             if line == "timestamp\ttext":
                 continue
-            timestamp, text = tuple(line.split(\t))
+            timestamp, text = tuple(line.split("\t"))
             text = text.replace("\\n", "\n").strip()
             if text == "":
                 continue
             tokens = tagger.pos(text, norm=True)
+            self._tokens.append(tokens[:])
             for token in tokens:
                 self._totalcount += 1
-                if token in self._freq:
+                if token in self._count:
                     self._count[token] += 1
                 else:
                     self._count[token] = 1
-        self._freq = {token: cnt / self._totalcount for token, cnt in self._count.iteritems()}
-            
+        self._freq = {token: cnt / self._totalcount for token, cnt in self._count.items()}
+        for key in sorted(self._freq.keys()):
+            targetfile.write(str(key) + "\t" + str(self._freq[key]) + "\n")
+
+    def get_dataset(self, targetfile, window_size = 3):
+        if self._tokens == []:
+            tagger = Twitter()
+            self.file.seek(0)
+            for line in self.file:
+                if line == "timestamp\ttext":
+                    continue
+                timestamp, text = tuple(line.split("\t"))
+                text = text.replace("\\n", "\n").strip()
+                if text == "":
+                    continue
+                tokens = tagger.pos(text, norm=True)
+                self._tokens.append(tokens[:])
+        for line in self._tokens:
+            for i in range(len(line)):
+                for j in range(window_size):
+                    if i-j-1 < 0:
+                        break
+                    targetfile.write(str(line[i]) + "\t" + str(line[i-j-1]) + "\n")
+                for j in range(window_size):
+                    if i+j+1 >= len(line):
+                        break
+                    targetfile.write(str(line[i]) + "\t" + str(line[i+j+1]) + "\n")
+                
